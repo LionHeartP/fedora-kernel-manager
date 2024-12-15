32d850542f87d3a2c783077117ee4dacd1b83e62
LionHeartP <parrr85@gmail.com>
2024-12-15 16:43:03 +0200

    Fix latest kernel detection

--- data/scripts/generate_package_info.sh
+++ data/scripts/generate_package_info.sh
@@ -2,7 +2,7 @@
 
 if [[ $1 == "version" ]]
 then
-	dnf info $2 2> /dev/null | grep Version | cut -d":" -f2 | sed -n 4p
+	dnf info $2 2> /dev/null | sed -e '/Available/,$!d' | grep Version | cut -d":" -f2 | head -n1
 elif [[ $1 == "description" ]]
 then
 	dnf info $2 2> /dev/null | sed -n '/Description/,/^$/p' | awk 'NR==1,/^$/ {if (/^$/ && printed) exit; if (NF) printed=1; if (NR==1) sub(/Description *: */, ""); else sub(/^ *: */, ""); print}'
